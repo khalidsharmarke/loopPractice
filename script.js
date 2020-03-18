04bc1b877fb486518d939a7ae16deca74513140d
khalidsharmarke <ksharmarke95@gmail.com>
2020-03-17 20:01:14 -0400

    done

---
+++ script.js
@@ -0,0 +1,26 @@
+for (let i = 0; i < 10; i++) {
+    console.log("the number is".toUpperCase(), i)
+}
+
+
+let x = 0;
+for (let i = 5; i < 16; i++) {
+    if (x % 3 == 0) {
+        console.log(i);
+    }
+    x++;
+}
+
+for(let i = 40; i < 61; i++){
+	alert(i + "\n" + "little monkeys sitting in a tree");
+	i++;
+}
+
+for(let i = 100; i > -1; i--){
+	console.log(i);
+}
+
+const love = "Boston CodeSquad".length
+for(let i = 0; i < love; i++){
+	console.log("I love JS")
+}
